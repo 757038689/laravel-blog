66c4fd436387281b5930666e9df30e1a332d2107
xiao北 <757038689@qq.com>
2020-04-12 11:35:12 +0800

    '微博app代码提交'

--- routes/web.php
+++ routes/web.php
@@ -13,13 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'StaticPagesController@home');
+Route::get('/help', 'StaticPagesController@help');
+Route::get('/about', 'StaticPagesController@about');
 
 
-Route::get('user/add', 'UserController@add');
-Route::get('user/find', 'UserController@find');
 
 Auth::routes();
 

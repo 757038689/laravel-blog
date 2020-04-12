0bd79c538600594d92b6d65d8b8288cf18117e98
xiao北 <757038689@qq.com>
2020-04-12 16:22:21 +0800

    '登陆和退出功能'

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,29 +3,38 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\User;
+use App\Models\User;
+use Auth;
 
 class UserController extends Controller
 {
-    //
-    public function add()
+    public function show(User $user)
     {
-        $res = User::create([
-            'name'=>'李使命',
-            'password'=>md5('123456'),
-            'email'=>'757038689@qq.com',
-        ]);
-
-        if ($res) {
-            echo 'ok';
-        }
+        return view('user.show', compact('user'));
     }
 
-    public function find()
+    public function create()
     {
+        return view('user.create');
+    }
 
-        $res = User::find(['id'=>1]);
-        echo '<pre>';
-        print_r($res);
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|unique:users|max:50',
+            'email' => 'required|email|unique:users|max:255',
+            'password' => 'required|confirmed|min:6'
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+
+        Auth::login($user);
+        session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
+        return redirect()->route('user.show', [$user]);
+
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -13,12 +13,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', 'StaticPagesController@home');
+Route::get('/', 'StaticPagesController@home')->name('home');
 Route::get('/help', 'StaticPagesController@help')->name('help');
 Route::get('/about', 'StaticPagesController@about')->name('about');
 
+Route::get('signup', 'UserController@create')->name('signup');
+Route::resource('user', 'UserController');
 
+Route::get('login', 'SessionsController@create')->name('login');
+Route::post('login', 'SessionsController@store')->name('login');
+Route::delete('logout', 'SessionsController@destroy')->name('logout');
 
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
+//Auth::routes();
+//
+//Route::get('/home', 'HomeController@index')->name('home');

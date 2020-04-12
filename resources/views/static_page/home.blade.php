b5e9122ebb8bf1e44070e94d7babcf9a84877dfc
xiao北 <757038689@qq.com>
2020-04-12 12:22:17 +0800

    '视图分块'

--- resources/views/static_page/home.blade.php
+++ resources/views/static_page/home.blade.php
@@ -2,5 +2,16 @@
 @section('title', '主页')
 
 @section('content')
-    <h1>主页</h1>
+    <div class="jumbotron">
+        <h1>你好，今天是:{{date('Y-m-d')}}</h1>
+        <p class="lead">
+            你现在所看到的是 Boegoby个人微博 主页。
+        </p>
+        <p>
+            精彩，将从这里开始。
+        </p>
+        <p>
+            <a class="btn btn-lg btn-success" href="#" role="button">现在注册</a>
+        </p>
+    </div>
 @stop
